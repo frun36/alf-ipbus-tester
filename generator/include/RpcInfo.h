b8ef4ac7bbfebe34c75c502a943aad2172c9dbed
frun36 <franek.urbanski@gmail.com>
2024-08-01 16:33:36 +0200

    Move RpcInfo to separate file

---
+++ generator/include/RpcInfo.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <dim/dic.hxx>
+
+#include <mutex>
+#include <condition_variable>
+
+class RpcInfo: public DimUpdatedInfo {
+    std::mutex& m_mtx;
+    std::condition_variable& m_cv;
+    bool& m_isDataReceived;
+    std::string& m_receivedData;
+    
+public:
+    RpcInfo(std::mutex& mtx, std::condition_variable& cv, bool& isDataReceived, std::string& receivedData)
+        : DimUpdatedInfo("ALF_FTM/SERIAL_0/LINK_0/SWT_SEQUENCE/RpcOut", -1), 
+          m_mtx(mtx), 
+          m_cv(cv), 
+          m_isDataReceived(isDataReceived), 
+          m_receivedData(receivedData) {}
+
+    void infoHandler() {
+        std::lock_guard<std::mutex> lock(m_mtx);
+        m_receivedData = getString();
+        m_isDataReceived = true;
+        m_cv.notify_one();
+    }
+};
--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -3,29 +3,8 @@
 #include <mutex>
 #include <condition_variable>
 
-#include <swt.h>
-
-class RpcInfo: public DimUpdatedInfo {
-    std::mutex& m_mtx;
-    std::condition_variable& m_cv;
-    bool& m_isDataReceived;
-    std::string& m_receivedData;
-    
-public:
-    RpcInfo(std::mutex& mtx, std::condition_variable& cv, bool& isDataReceived, std::string& receivedData)
-        : DimUpdatedInfo("ALF_FTM/SERIAL_0/LINK_0/SWT_SEQUENCE/RpcOut", -1), 
-          m_mtx(mtx), 
-          m_cv(cv), 
-          m_isDataReceived(isDataReceived), 
-          m_receivedData(receivedData) {}
-
-    void infoHandler() {
-        std::lock_guard<std::mutex> lock(m_mtx);
-        m_receivedData = getString();
-        m_isDataReceived = true;
-        m_cv.notify_one();
-    }
-};
+#include "swt.h"
+#include "RpcInfo.h"
 
 int main(void) {
     std::mutex mtx;
