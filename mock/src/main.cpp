c87a6a68a1fd60aeeb96672b015bf4b3d3b22e7e
frun36 <franek.urbanski@gmail.com>
2024-08-22 14:49:01 +0200

    Basic callback

--- mock/src/main.cpp
+++ mock/src/main.cpp
@@ -1,12 +1,22 @@
 #include "Mock.h"
 #include "IPbusSlave.h"
+#include "IPbusRequest.h"
 
 #include <boost/asio.hpp>
 
+#include "logging.h"
+
 int main(void) {
+    logging::init();
+
     boost::asio::io_context io;
     Mock mock("ftm_registers.csv");
     ipbus::IPbusSlave slave(io, mock, 50001);
+
+    slave.setRequestCallback([](const ipbus::IPbusRequest& req) {
+        std::cout << "Received words: " << req.getSize() << "\n";
+    });
+
     while(true) 
         io.run();
 
