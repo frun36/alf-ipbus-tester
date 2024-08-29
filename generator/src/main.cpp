3483250e8822c31514fd3f9352c1f2a2e0e156d2
frun36 <franek.urbanski@gmail.com>
2024-08-29 16:08:29 +0200

    Add round trip time measurement to generator

--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -3,6 +3,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <boost/log/trivial.hpp>
+#include <chrono>
 
 #include "swt.h"
 #include "RpcInfo.h"
@@ -43,29 +44,38 @@ int main(int argc, const char** argv) {
             BOOST_LOG_TRIVIAL(info) << "Performing test \"" << test.name << "\"";
 
             for(size_t i = 0; i < test.repeats; i++) {
+                double rttSum = 0;
+
                 for(const auto& seq : test.sequences) {    
                     std::string seqStr = seq.getRequest();
 
                     BOOST_LOG_TRIVIAL(debug) << "Sending data:\n" << seqStr;
+
+                    auto start = std::chrono::high_resolution_clock::now();
                     DimClient::sendCommand(rpcInName.c_str(), seqStr.c_str());
 
                     std::unique_lock<std::mutex> lock(mtx);
                     cv.wait(lock, [&isDataReceived]{ return isDataReceived; });
 
                     // Process the received data
+                    auto stop = std::chrono::high_resolution_clock::now();
+                    std::chrono::duration<double, std::micro> elapsed = stop - start;
+                    double microseconds = elapsed.count();
+                    rttSum += microseconds;
+
                     BOOST_LOG_TRIVIAL(debug) << "Received data:\n" << receivedData << "\n";
 
                     isDataReceived = false;
                     bool result = SwtSequence::match(seq.getSuccessResponse(), receivedData);
                     if (result == test.sequenceResponses[seqId])
-                        BOOST_LOG_TRIVIAL(debug) << test.name << ": success (repeat " << i << ", seqId " << seqId << ")";
+                        BOOST_LOG_TRIVIAL(debug) << test.name << ": success (repeat " << i << ", seqId " << seqId << ", RTT " << microseconds << " us)";
                     else {
-                        BOOST_LOG_TRIVIAL(error) << test.name << ": failure (repeat " << i << ", seqId " << seqId << ", expected " << test.sequenceResponses[seqId] << ")\n--- Sent ---\n" << seqStr << "\n--- Received ---\n" << receivedData;
+                        BOOST_LOG_TRIVIAL(error) << test.name << ": failure (repeat " << i << ", seqId " << seqId << ", RTT " << microseconds << " us, expected " << test.sequenceResponses[seqId] << ")\n--- Sent ---\n" << seqStr << "\n--- Received ---\n" << receivedData;
                         exit(1);
                     }
                     seqId++;
                 }
-                BOOST_LOG_TRIVIAL(info) << test.name << ": success " << i+1 << "/" << test.repeats;
+                BOOST_LOG_TRIVIAL(info) << test.name << ": success " << i+1 << "/" << test.repeats << ", mean RTT: " << rttSum / test.sequences.size() << " us";
                 usleep(test.wait);
             }
         }
