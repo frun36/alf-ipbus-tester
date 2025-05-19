bd3ebc5028260382159eb6ace87058ca61865b71
Franciszek Urbański <118205167+frun36@users.noreply.github.com>
2025-05-19 13:48:34 +0200

    Revert "9 fred compatibility"

--- shared/include/Config.h
+++ shared/include/Config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <error.h>
+#include <exception>
 #include <string>
 #include <vector>
 
@@ -9,10 +9,20 @@
 #include "toml++/toml.hpp"
 
 struct Config {
+    struct Exception : public std::runtime_error {
+        Exception(const std::string& msg)
+            : std::runtime_error(msg) {
+        }
+    };
+
     static std::string help;
 
     GlobalConfig global;
     std::vector<TestConfig> tests;
 
-    static Result<Config> readFile(std::string filename);
+    static Config readFile(std::string filename);
+
+
+   private:
+    Config(const toml::table& tbl);
 };
